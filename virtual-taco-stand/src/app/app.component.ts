60c4003eeb5259511da35fec89f7b2cf7b5d987f
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-09-09 15:55:47 -0700

    Add Reactive Forms with TDD

--- virtual-taco-stand/src/app/app.component.ts
+++ virtual-taco-stand/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
       </header>
 
       <div class="sign-in-container">
-        <a routerLink="/signin" class="sign-in-link">Sign In</a>
+        @if (email) {
+          <p>Welcome, {{ email }}!</p>
+          <button (click)="signout()">Sign Out</button>
+        } @else {
+          <a routerLink="/signin" class="sign-in-link">Sign In</a>
+        }
       </div>
 
       <main class="main-content">
@@ -66,5 +73,23 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   ]
 })
 export class AppComponent {
+  email?: string;
 
+  constructor(
+    private authService: AuthService,
+    private cookieService: CookieService
+  ) {
+  }
+
+  ngOnInit() {
+    this.authService.getAuthState().subscribe((isAuth) => {
+      if (isAuth) {
+        this.email = this.cookieService.get('session_user');
+      }
+    });
+  }
+
+  signout() {
+    this.authService.signout();
+  }
 }
